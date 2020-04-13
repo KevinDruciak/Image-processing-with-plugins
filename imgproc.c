70e50fed91f036f961fd6e9f420b2114fd0aa39c
Kevin Druciak <kevintdruciak@gmail.com>
2020-04-13 01:46:37 -0400

    finished imgproc

--- imgproc.c
+++ imgproc.c
@@ -4,6 +4,9 @@
 #include <dirent.h>
 #include <sys/types.h>
 #include <stddef.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "image.h"
 
 struct Plugin {
   void *handle;
@@ -11,59 +14,144 @@ struct Plugin {
   const char *(*get_plugin_desc)(void);
   void *(*parse_arguments)(int num_args, char *args[]);
   struct Image *(*transform_image)(struct Image *source, void *arg_data);
-  int temp;
 };
 
 int get_directory_size() {
   int file_count = 0;
-  //use getenv to check for PLUGIN_DIR first
-  DIR * dir = opendir("./plugins");
+  char *path = getenv("PLUGIN_DIR");
+  if (path == NULL) {
+    path = "./plugins";
+  }
+  DIR * dir;
+  if ((dir = opendir(path)) == NULL) {
+    if (closedir(dir) != 0) {
+      printf("Error: unable to close directory");
+      exit(1);
+    }
+    printf("Error: cannot open directory");
+    exit(1);
+  }
   struct dirent * dr;
   while ((dr = readdir(dir)) != NULL) {
     file_count++;
   }
-  closedir(dir);
-  return file_count;
+  if (closedir(dir) != 0) {
+    printf("Error: unable to close directory");
+    exit(1);
+  }
+  return file_count - 2;
 }
 
-void find_files(struct Plugin plugins[], size_t len) {
-  //use getenv to check for PLUGIN_DIR first
-  char * dir_name = "./plugins";
-  DIR * dir = opendir(dir_name);
-  //error if null dir_name
+bool validate_file(char *file) {
+  int file_len = strlen(file);
+  char *temp = strstr(file, ".so");
+  if (strcmp(file, ".") == 0 || strcmp(file, "..") == 0 || temp - file != file_len - 3) {
+    return false;
+  }
+  return true;
+}
+
+void list() {
+  printf("Loaded %d plugin(s)\n", get_directory_size());
+  char *path = getenv("PLUGIN_DIR");
+  if (path == NULL) {
+    path = "./plugins";
+  }
+  DIR * dir;
+  if ((dir = opendir(path)) == NULL) {
+    closedir(dir);
+    printf("Error: cannot open directory");
+    exit(1);
+  }
   struct dirent * dr;
-  size_t index = 0;
   while ((dr = readdir(dir)) != NULL) {
     char * file = dr->d_name;
-    char fullpath[1000];
-    snprintf(fullpath, 1000, "%s/%s", dir_name, dr->d_name);
-    fullpath[999] = '\0';
-    puts(fullpath);
-    void * handle = dlopen(fullpath, RTLD_LAZY);
-    plugins[index].temp = index;
-    index++;
+    if (validate_file(file)) {
+      char fullpath[1000];
+      snprintf(fullpath, 1000, "%s/%s", path, dr->d_name);
+      fullpath[999] = '\0';
+      struct Plugin plugin;
+      plugin.handle = dlopen(fullpath, RTLD_LAZY);
+      *(void **) &(plugin.get_plugin_name) = dlsym(plugin.handle, "get_plugin_name");
+      *(void **) &(plugin.get_plugin_desc) = dlsym(plugin.handle, "get_plugin_desc");
+      printf("%s: %s\n", plugin.get_plugin_name(), plugin.get_plugin_desc());
+      dlclose(plugin.handle);
+    }
   }
-  //puts(fullpath);
   closedir(dir);
 }
 
-int main (int argc, char *argv[1]) {
-  size_t dir_size = get_directory_size();
-  struct Plugin plugins[dir_size - 2];
-  //change this to use normal sizes lol???
-  //find_files(plugins, sizeof(plugins) / sizeof(*plugins));
-  find_files(plugins, dir_size - 2);
-
-  for (size_t i = 0; i < dir_size; i++) {
-    printf("%zu: %d\n", i, plugins[i].temp);
+void exec(int argc, char *argv[]) {
+  char *path = getenv("PLUGIN_DIR");
+  if (path == NULL) {
+    path = "./plugins";
   }
-  
+  DIR * dir;
+  if ((dir = opendir(path)) == NULL) {
+    closedir(dir);
+    printf("Error: cannot open directory");
+    exit(1);
+  }
+  struct dirent * dr;
+  while ((dr = readdir(dir)) != NULL) {
+    if (validate_file(dr->d_name)) {
+      char fullpath[1000];
+      snprintf(fullpath, 1000, "%s/%s", path, dr->d_name);
+      fullpath[999] = '\0';
+      struct Plugin plugin;
+      plugin.handle = dlopen(fullpath, RTLD_LAZY);
+      *(void **) &(plugin.get_plugin_name) = dlsym(plugin.handle, "get_plugin_name");
+      if (strcmp(plugin.get_plugin_name(), argv[2]) == 0) {
+	*(void **) &(plugin.parse_arguments) = dlsym(plugin.handle, "parse_arguments");
+	int index = 5;
+	char *args[1000];
+	while (argv[index] != NULL) {
+	  args[index - 5] = argv[index];
+	  index++;
+	}
+	*(void **) &(plugin.transform_image) = dlsym(plugin.handle, "transform_image");
+	struct Image *source = img_read_png(argv[3]);
+	if (source == NULL) {
+	  printf("Error: unable to load image");
+	  exit(1);
+	}
+	struct Image *out = plugin.transform_image(source, plugin.parse_arguments(index - 4, args));
+	if (out == NULL) {
+	  printf("Error: unable to output image");
+	  exit(1);
+	}
+	if (img_write_png(out, argv[4]) == 0) {
+	  printf("Error: unable to write image to file");
+	  exit(1);
+	}
+	img_destroy(source);
+	img_destroy(out);
+	dlclose(plugin.handle);
+	break;
+      }
+      dlclose(plugin.handle);
+    }
+  }
+  closedir(dir);
+}
+
+
+int main (int argc, char *argv[]) {
+  if (argc < 2) {
+    printf("Usage: imgproc <command> [<command args...>]\nCommands are:\n \tlist\n \texec <plugin> <input img> <output img> [<plugin args...>]\n");
+    exit(1);
+  }
   if (strcmp(argv[1], "list") == 0) {
-    //list();
+    list();
   } else if (strcmp(argv[1], "exec") == 0) {
-    //findFiles();
+    if (argc < 5) {
+      printf("Usage: imgproc <command> [<command args...>]\nCommands are:\n \tlist\n \texec <plugin> <input img> <output img> [<plugin args...>]\n");
+      exit(1);
+    }
+    exec(argc, argv);
   } else {
-    return 1; //bad param somewhere
+    printf("Usage: imgproc <command> [<command args...>]\nCommands are:\n \tlist\n \texec <plugin> <input img> <output img> [<plugin args...>]\n");
+    exit(1);
   }
   return 0;
 }
